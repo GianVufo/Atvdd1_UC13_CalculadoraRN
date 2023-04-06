918ef9a8cb0802e44d5937070555e06219ebdb75
Gianluca-Vialli <ginaluca19993m@gmail.com>
2023-04-06 19:51:47 -0300

    Alteração de layput atendendo a melhoria de correção no estilo da aplicação.

--- App.js
+++ App.js
@@ -78,12 +78,12 @@ export default function App() {
 
         {buttons.map((button) =>
           button === '=' ? // Mapeamento do botão =
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#3dd0e3' }]}>
-              <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#21024E' }]}>
+              <Text style={[styles.textButton, { color: "#fff", fontSize: 30 }]}>{button}</Text>
             </TouchableOpacity>
             : // Mapeamento dos outros botões
             <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6' }]}>{button}</Text>
+              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? '#fff' : '#A5AAA3' }]}>{button}</Text>
             </TouchableOpacity>
         )}
       </View>
@@ -99,17 +99,17 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#21024E"
   },
   resultText: {
-    color: "#282F38",
+    color: "#fff",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
   },
   historyText: {
-    color: "#7c7c7c",
+    color: "#a9a9a9",
     fontSize: 20,
     marginRight: 10,
     alignSelf: 'flex-end',
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3D0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90,
@@ -127,7 +127,6 @@ const styles = StyleSheet.create({
     flex: 2,
   },
   textButton: {
-    color: "#7c7c7c",
     fontSize: 20,
   }
 });
