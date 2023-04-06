52ccac3bc7f321c834a3f858def26c31be6a6e8a
Gianluca-Vialli <ginaluca19993m@gmail.com>
2023-04-06 19:24:45 -0300

    Corrigindo novamente o símbólo de multiplicação para evitar o erro de cálculo.

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
       case '-':
         setCurrentNumber((fistNumber - lastNumber).toString())
         return
-      case '*':
+      case 'x':
         setCurrentNumber((fistNumber * lastNumber).toString())
         return
       case '/':
